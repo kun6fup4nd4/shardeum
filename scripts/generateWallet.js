ff19d055784090597dada61987c9d606ca993229
kun6fup4nd4 <tim@shardeum.org>
2025-02-15 09:50:25 -0400

    third cleanup

--- scripts/generateWallet.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('@shardeum-foundation/lib-crypto-utils');
-
-// Generate a random keypair
-const keypair = crypto.generateKeypair();
-
-// Print the generated keypair to the console
-console.log('Public Key:', keypair.publicKey);
-console.log('Secret Key:', keypair.secretKey);
